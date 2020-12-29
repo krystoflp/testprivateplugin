cd7ace944d66dee2494f9f641d35e5ea5a66eb53
krystoflp <72083709+krystoflp@users.noreply.github.com>
2020-12-29 21:30:07 +0000

    Create testprivateplugin.php

---
+++ testprivateplugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: testprivateplugin
+Plugin URI: https://ChurchWeb.uk
+Description: Tester for auto updater from private gothiub repository
+Version: 2.0.3
+Author: ChurchWeb
+
+Author URI: https://ChurchWeb.uk
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) exit;
