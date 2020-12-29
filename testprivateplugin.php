a8627268074b9faa762f69aa3ff79686a129575d
krystoflp <72083709+krystoflp@users.noreply.github.com>
2020-12-29 22:45:14 +0000

    Update testprivateplugin.php

--- testprivateplugin.php
+++ testprivateplugin.php
@@ -11,3 +11,17 @@ Author URI: https://ChurchWeb.uk
 
 // If this file is called directly, abort.
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Run update code
+require 'CWupdater/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/krystoflp/testprivateplugin',
+	__FILE__,
+	'testprivateplugin'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('e3ec7722722afe04e67f8c566cce487517232668');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('stable-branch-name');
