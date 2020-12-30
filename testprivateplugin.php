9e4a5d79a2ca18bcbe6eaed11139515ae2c44aa4
krystoflp <72083709+krystoflp@users.noreply.github.com>
2020-12-30 11:09:56 +0000

    Update testpluginupdater.php
    remove private access key. add release functionality in github

--- testprivateplugin.php
+++ testprivateplugin.php
@@ -20,8 +20,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'testprivateplugin'
 );
 
-//Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication('e3ec7722722afe04e67f8c566cce487517232668');
 
-//Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('stable-branch-name');
+//Optional: Enable github release functionality
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
+
