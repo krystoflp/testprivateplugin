337fecee634ddad1e51b6775220fa8cd7ba63a6d
krystoflp <72083709+krystoflp@users.noreply.github.com>
2020-12-30 12:26:35 +0000

    Update testprivateplugin.php

--- testprivateplugin.php
+++ testprivateplugin.php
@@ -3,7 +3,7 @@
 Plugin Name: testprivateplugin
 Plugin URI: https://ChurchWeb.uk
 Description: Tester for auto updater from private githib repository
-Version: 0.0.10
+Version: 0.0.11
 Author: ChurchWeb
 
 Author URI: https://ChurchWeb.uk
